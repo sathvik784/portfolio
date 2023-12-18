30644084a19c40fe8162683696db95f7d2f7a513
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2023-12-17 23:53:48 -0500

    edits

--- src/components/SideProjectsCard.js
+++ src/components/SideProjectsCard.js
@@ -1,12 +1,12 @@
 import React from "react";
+import '../styles/ProjectCard.css'
 
 const SideProjectCard = ({projectData}) => {
     return(
         <div>
             <h1>{projectData.Name}</h1>
-
             <p>{projectData.Description}</p>
-            <a href="{projectData.GitHubLink}">
+            <a href={projectData.GitHubLink}>
                 <button>GitHub</button>
             </a>
         </div>
