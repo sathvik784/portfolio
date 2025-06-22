3ed6b0677ce9c29260cec64f8bce714899ac7e8d
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2025-06-21 22:46:40 -0400

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
         <nav>
             <ul>
                 <li>
-                    <Link to='/'>Home</Link>
+                    <Link to='/portfolio'>Home</Link>
                 </li>
                 <li>
                     <Link to='/about'>About</Link>
