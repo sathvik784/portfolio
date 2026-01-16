8063915ccc2694965252ef72d8a395f17dfebfa0
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2026-01-16 17:22:33 -0500

    .

--- src/components/SideProjectsData.js
+++ src/components/SideProjectsData.js
@@ -1,27 +1,27 @@
 const projectData = [
-    {
-        "id": 1,
-        "Name": "Honeypot Project",
-        "Description": "Created a honeypot system to study how different SSH banners influence attacker behavior and track command execution patterns.",
-        "GitHub": "https://github.com/HACS200-HoneyPot-Project",
+  {
+		"id": 1,
+		"Name": "Honeypot Project",
+		"Description": "Created a honeypot system to study how different SSH banners influence attacker behavior and track command execution patterns.",
+		"GitHub": "https://github.com/HACS200-HoneyPot-Project",
 		"PDFLink": "https://docs.google.com/document/d/1n3fuHgBWw82nLBBH5Km4L7ShGSEvoisJoTFg5Bd_mb0/export?format=pdf",
 		"techStack": ["Bash", "Unix/Linux", "Python"]
-    },
-    {
-        "id": 2,
-        "Name": "Custom Fantasy Football Platform",
-        "Description": "A custom fantasy football platform with user authentication, real-time league management, and player statistics tracking.",
-        "GitHub": "https://github.com/Fantasy-Football-project",
-		"techStack": ["React", "JavaScript", "Spring Boot", "Java", "PostgreSQL"]
-    },
+  },
 	{
-		"id": 3,
+		"id": 2,
 		"Name": "Playoff Pickems' Project",
 		"Description": "A web application for NFL playoff predictions where users compete against friends by picking props and betting outcomes.",
 		"GitHub": "https://github.com/Playoff-PickEm-Project",
 		"LiveDemo": "https://playoff-pickem-frontend-q31n.onrender.com/",
 		"techStack": ["React", "JavaScript", "Flask", "Python", "PostgreSQL"]
 	},
+	{
+		"id": 3,
+		"Name": "Custom Fantasy Football Platform",
+		"Description": "A custom fantasy football platform with user authentication, real-time league management, and player statistics tracking.",
+		"GitHub": "https://github.com/Fantasy-Football-project",
+		"techStack": ["React", "JavaScript", "Spring Boot", "Java", "PostgreSQL"]
+  },
 	{
 		"id": 4,
 		"Name": "Fantasy Football Production Analysis",
